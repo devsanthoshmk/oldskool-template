15abbe24893265762d52eb0be343f9a77b25dfc3
devsanthoshmk <kavimanisan@gmail.com>
2024-12-30 09:21:02 +0530

    htmls added to django templates

--- templ-django/templete/products/views.py
+++ templ-django/templete/products/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "products/index.html")
+
+
+def product(request):
+    return render(request, "products/product.html")
