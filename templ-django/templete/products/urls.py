13078ab451053637f24e56edbc0d176531c913e7
devsanthoshmk <kavimanisan@gmail.com>
2024-12-31 05:07:04 +0530

    added dunamic placeholder for index.html

--- templ-django/templete/products/urls.py
+++ templ-django/templete/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
     path("product", views.product, name="product"),
     path("cart", views.cart, name="cart"),
     path("checkout", views.checkout, name="checkout"),
