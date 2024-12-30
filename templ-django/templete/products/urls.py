f15b51b0a5be60c21b7b52da55fe049ea5378c98
devsanthoshmk <kavimanisan@gmail.com>
2024-12-30 20:20:22 +0530

    solved conflict and nav set

--- templ-django/templete/products/urls.py
+++ templ-django/templete/products/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("product", views.product, name="product"),
-    path("cart", views.product, name="product"),
+    path("cart", views.cart, name="cart"),
+    path("checkout", views.checkout, name="checkout"),
+    path("checkout-shipping", views.checkout_shipping, name="checkout-shipping"),
+    path("checkout-payment", views.checkout_payment, name="checkout-payment"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("forgotten-password", views.forgotten_password, name="forgotten-password"),
 ]
--- templ-django/templete/products/views.py
+++ templ-django/templete/products/views.py
@@ -8,3 +8,31 @@ def index(request):
 
 def product(request):
     return render(request, "products/product.html")
+
+
+def cart(request):
+    return render(request, "products/cart.html")
+
+
+def checkout(request):
+    return render(request, "products/checkout.html")
+
+
+def checkout_shipping(request):
+    return render(request, "produstc/checkout-shipping.html")
+
+
+def checkout_payment(request):
+    return render(request, "products/checkout-payment.html")
+
+
+def login(request):
+    return render(request, "products/login.html")
+
+
+def register(request):
+    return render(request, "products/register.html")
+
+
+def forgotten_password(request):
+    return render(request, "products/forgotten-password.html")
