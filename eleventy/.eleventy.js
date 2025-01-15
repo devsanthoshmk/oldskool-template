1fc9c433bc0153a71e7a4d8b45994d342f6fcd09
devsanthoshmk <kavimanisan@gmail.com>
2025-01-16 03:55:18 +0530

    implementing .json datas in index.html

--- eleventy/.eleventy.js
+++ eleventy/.eleventy.js
@@ -21,29 +21,37 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('static/assets/images/products');
 
     // JS files
-    eleventyConfig.addPassthroughCopy('static/assets/js/route.js');
+    eleventyConfig.addWatchTarget('static/assets/js/route.js');
     eleventyConfig.addPassthroughCopy('static/assets/js/theme.bundle.js');
     eleventyConfig.addPassthroughCopy('static/assets/js/vendor.bundle.js');
    
     // SVGs folder
     eleventyConfig.addPassthroughCopy('static/assets/svgs');
 
+    //for jsons in data folder
+    // eleventyConfig.addWatchTarget('./data'); // Add your new data directory
+
     // Define a custom shortcode for static files
     eleventyConfig.addNunjucksShortcode('static', (filePath) => {
+        if ("http"===filePath.substring(0, 4))
+            return filePath;
+        // console.log(filePath);
         return `/static/${filePath}`;
     });
 
-     eleventyConfig.addPairedShortcode('comment', (content) => {
+    eleventyConfig.addPairedShortcode('comment', (content) => {
         // Simply ignore the content inside the "comment" tag
         return '';
     });
+
 
     // Customize input and output directories
     return {
         dir: {
             input: 'templates',     // Templates folder
             includes: 'includes',    // Includes folder within templates
-            output: 'dist',         // Output folder for the generated site
+            output: 'dist',        // Output folder for the generated site
+            data: 'data'
         },
         htmlTemplateEngine: 'njk', // Use Nunjucks for HTML files
     };
